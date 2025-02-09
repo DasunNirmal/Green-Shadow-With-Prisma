6debc706ca031b18c9e61a14a03fd70655de5e80
DasunNirmal <dasun200301@gmail.com>
2025-02-09 23:20:34 +0530

    fields done

--- routes/field-routes.ts
+++ routes/field-routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import Field from "../model/Field";
-import {addFields} from "../database/field-data-store";
+import {addFields, deleteFields, getAllFields, updateFields} from "../database/field-data-store";
 import multer from "multer";
 
 const router = express.Router();
@@ -30,6 +30,49 @@ router.post('/add', upload.fields([{ name: 'img_01', maxCount: 1 }, { name: 'img
     } catch (error) {
         console.error(error);
         res.status(500).send('Error adding fields');
+    }
+});
+
+router.delete('/delete/:field_code', async (req, res) => {
+    const field_code = req.params.field_code;
+    try {
+        await deleteFields(field_code);
+        res.send('Fields deleted successfully');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error deleting fields');
+    }
+});
+
+router.get('/get', async (req, res) => {
+    try {
+        const fields = await getAllFields();
+        res.json(fields);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching fields');
+    }
+});
+
+router.put('/update/:field_code', upload.fields([{ name: 'img_01', maxCount: 1 }, { name: 'img_02', maxCount: 1 },]), async (req, res) => {
+    const field_code = req.params.field_code;
+    const fields: Field = req.body;
+
+    // Access uploaded files
+    const files = req.files as { [fieldName: string]: Express.Multer.File[] };
+    const img_01 = files['img_01']?.[0]?.buffer.toString('base64'); // Convert to base64
+    const img_02 = files['img_02']?.[0]?.buffer.toString('base64'); // Convert to base64
+
+    // Add image data to the fields object
+    fields.img_01 = img_01 || '';
+    fields.img_02 = img_02 || '';
+
+    try {
+        await updateFields(field_code, fields);
+        res.send('Fields updated successfully');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error updating fields');
     }
 });
 
