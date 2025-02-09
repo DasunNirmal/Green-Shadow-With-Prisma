5fd92e0f6c0123831854f0a15e1bca35acaf748f
DasunNirmal <dasun200301@gmail.com>
2025-02-09 23:20:27 +0530

    fields done

--- database/field-data-store.ts
+++ database/field-data-store.ts
@@ -21,3 +21,39 @@ export async function addFields(fields: Field) {
         console.log('Error Adding Fields',e);
     }
 }
+
+export async function deleteFields(field_code: string) {
+    try {
+        await prisma.field.delete({
+            where: {field_code: field_code}
+        });
+    } catch (e) {
+        console.log('Error Deleting Fields',e);
+    }
+}
+
+export async function getAllFields() {
+    try {
+        return await prisma.field.findMany();
+    } catch (e) {
+        console.log('Error Getting Fields',e);
+    }
+}
+
+export async function updateFields(field_code: string, fields: Field) {
+    try {
+        await prisma.field.update({
+            where: {field_code: field_code},
+            data: {
+                field_code: fields.field_code,
+                extent_size: fields.extent_size,
+                field_location: fields.field_location,
+                field_name: fields.field_name,
+                img_01: fields.img_01,
+                img_02: fields.img_02
+            }
+        });
+    } catch (e) {
+        console.log('Error Updating Fields',e);
+    }
+}
