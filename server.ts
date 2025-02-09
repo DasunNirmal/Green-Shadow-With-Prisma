9ee4a989a3da6d4294073243440be79f34422cb5
DasunNirmal <dasun200301@gmail.com>
2025-02-09 17:53:13 +0530

    field save not done yet

---
+++ server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import fieldRoutes from "./routes/field-routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+    origin: "http://localhost:5173",  // Allow frontend requests
+    methods: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept"
+}));
+
+app.use('/field', fieldRoutes);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
