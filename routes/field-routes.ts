e2d8c50e9c6421ac8e14422309d4056338422816
DasunNirmal <dasun200301@gmail.com>
2025-02-09 22:42:39 +0530

    save fields done

--- routes/field-routes.ts
+++ routes/field-routes.ts
@@ -5,8 +5,25 @@ import multer from "multer";
 
 const router = express.Router();
 
-router.post('/add', async (req, res) => {
+const upload = multer({
+    storage: multer.memoryStorage(), // Store files in memory as Buffer
+    limits: {
+        fileSize: 10 * 1024 * 1024, // Limit file size to 10MB
+    },
+});
+
+router.post('/add', upload.fields([{ name: 'img_01', maxCount: 1 }, { name: 'img_02', maxCount: 1 },]), async (req, res) => {
     const fields: Field = req.body;
+
+    // Access uploaded files
+    const files = req.files as { [fieldName: string]: Express.Multer.File[] };
+    const img_01 = files['img_01']?.[0]?.buffer.toString('base64'); // Convert to base64
+    const img_02 = files['img_02']?.[0]?.buffer.toString('base64'); // Convert to base64
+
+    // Add image data to the fields object
+    fields.img_01 = img_01 || '';
+    fields.img_02 = img_02 || '';
+
     try {
         await addFields(fields);
         res.send('Fields added successfully');
