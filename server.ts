d07c28b369e629a04f6ed9a418773a79b382d3d9
DasunNirmal <dasun200301@gmail.com>
2025-02-10 22:14:43 +0530

    staff added

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import fieldRoutes from "./routes/field-routes";
 import cropsRoutes from "./routes/crops-routes";
+import staffRoutes from "./routes/staff-routes";
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(cors({
 
 app.use('/field', fieldRoutes);
 app.use('/crops', cropsRoutes);
+app.use('/staff', staffRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
