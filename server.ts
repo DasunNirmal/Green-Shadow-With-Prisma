ced13f95b97bccd884c20cc3f82c146eb98bc743
DasunNirmal <dasun200301@gmail.com>
2025-02-10 11:50:16 +0530

    crops added

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import fieldRoutes from "./routes/field-routes";
+import cropsRoutes from "./routes/crops-routes";
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cors({
 }));
 
 app.use('/field', fieldRoutes);
+app.use('/crops', cropsRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
